09d361073e8b40cecccb7a5299b27380fb38f873
dkolepp <dk14142@gmail.com>
2020-10-12 10:44:32 -0400

    fix: update setup.py pylint version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pylint_shim',
     version='0.0.1',
-    install_requires=['pylint==1.9.5'],
+    install_requires=['pylint==2.2'],
     scripts=['pylint-shim.py'],
 )
 
